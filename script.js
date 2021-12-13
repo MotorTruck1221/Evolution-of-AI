063e36a52f66f50c9d30782049d34c8cf2edb28e
MotorTruck1221 <73721704+MotorTruck1221@users.noreply.github.com>
2021-12-13 14:01:55 -0700

    Re-did

--- script.js
+++ script.js
@@ -1,66 +1,15 @@
-$('.dot:nth-child(1)').click(function(){
-    $('.inside').animate({
-      'width' : '20%'
-    }, 500);
-  });
-  $('.dot:nth-child(2)').click(function(){
-    $('.inside').animate({
-      'width' : '40%'
-    }, 500);
-  });
-  $('.dot:nth-child(3)').click(function(){
-    $('.inside').animate({
-      'width' : '60%'
-    }, 500);
-  });
-  $('.dot:nth-child(4)').click(function(){
-    $('.inside').animate({
-      'width' : '80%'
-    }, 500);
-  });
-  if ($('#switch1').not(':checked')){
-    $('.modal').wrap('<div class="mask"></div>')
-      $('.mask').click(function(){
-        $(this).fadeOut(300);
-        $('.mask article').animate({
-          'top' : '-100%'
-        }, 300)
-      });
-  
-      $('.dot').click(function(){
-        var modal = $(this).attr('id');
-        $('.mask').has('article.' + modal).fadeIn(300);
-        $('.mask article.' + modal).fadeIn(0).animate({
-          'top' : '10%'
-        }, 300);
-      });
+// SMOOTH SCROLLING SECTIONS
+$('a[href*=#]:not([href=#])').click(function() {
+  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+      || location.hostname == this.hostname) {
+
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+         if (target.length) {
+           $('html,body').animate({
+               scrollTop: target.offset().top
+          }, 1000);
+          return false;
+      }
   }
-  $("#switch1").click(function(){
-    if ($('#switch1').is(':not checked')){
-      $('.modal').unwrap('<div class="mask"></div>');
-      $('.modal').hide();
-      $('.modal').addClass('nobox');
-      $('.dot').click(function(){
-      var modal = $(this).attr('id');
-      $('article.nobox').hide()
-      $('article.nobox.' + modal).fadeIn(200)
-      });
-    } else {
-      $('article').removeClass("nobox");
-      $('.modal').wrap('<div class="mask"></div>')
-      $('.mask').click(function(){
-        $(this).fadeOut(300);
-        $('.mask article').animate({
-          'top' : '-100%'
-        }, 300)
-      });
-  
-      $('.dot').click(function(){
-        var modal = $(this).attr('id');
-        $('.mask').has('article.' + modal).fadeIn(300);
-        $('.mask article.' + modal).fadeIn(0).animate({
-          'top' : '10%'
-        }, 300);
-      });
-    }
-  })
+});
